3d8172a10c5ab00fcb26e6cec8ad1d83a921e561
slow-learn-err <abpandey0791@gmail.com>
2021-10-23 23:16:49 +0530

    CLUB-20 - custom server with websocket

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const next = require('next');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  const server = express();
+
+  server.all('*', (req, res) => {
+    return handle(req, res)
+  });
+
+  const app = server.listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  });
+
+  const io = require("socket.io")(app, {
+    cors: {
+      origin: "*",
+    },
+  });
+  
+  const NEW_CHAT_MESSAGE_EVENT = "newChatMessage";
+  
+  io.on("connection", (socket) => {
+    console.log(`Client ${socket.id} connected`);
+  
+    // Join a conversation
+    const { roomId } = socket.handshake.query;
+    socket.join(roomId);
+  
+    // Listen for new messages
+    socket.on(NEW_CHAT_MESSAGE_EVENT, (data) => {
+      io.in(roomId).emit(NEW_CHAT_MESSAGE_EVENT, data);
+    });
+  
+    // Leave the room if the user closes the socket
+    socket.on("disconnect", () => {
+      console.log(`Client ${socket.id} diconnected`);
+      socket.leave(roomId);
+    });
+  });
+});

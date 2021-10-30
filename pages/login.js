6e875c57c4bb6b378b1e2a98f8ee8c90b00b9549
Arindam Ganguly <arindam.ganguly@seenit.in>
2021-10-30 16:32:49 +0530

    CLUB-27

---
+++ pages/login.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import Router from 'next/router'
+import Layout from '../components/layout'
+import Form from '../components/form'
+
+const Login = () => {
+  const [errorMsg, setErrorMsg] = useState('')
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    if (errorMsg) setErrorMsg('')
+
+    const body = {
+      email: e.currentTarget.email.value,
+      password: e.currentTarget.password.value,
+    }
+
+    try {
+        Router.push('/')
+    } catch (error) {
+      console.error('An unexpected error happened occurred:', error)
+      setErrorMsg(error.message)
+    }
+  }
+
+  return (
+    <Layout>
+      <div className="login">
+        <Form isLogin errorMessage={errorMsg} onSubmit={handleSubmit} />
+      </div>
+      <style jsx>{`
+        .login {
+          max-width: 21rem;
+          margin: 0 auto;
+          padding: 1rem;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+        }
+      `}</style>
+    </Layout>
+  )
+}
+
+export default Login
---
+++ pages/signup.js
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import Router from 'next/router'
+import Form from '../components/form'
+import Layout from '../components/layout'
+
+const Signup = () => {
+
+  const [errorMsg, setErrorMsg] = useState('')
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+
+    if (errorMsg) setErrorMsg('')
+
+    const body = {
+      email: e.currentTarget.email.value,
+      password: e.currentTarget.password.value,
+    }
+
+    if (body.password !== e.currentTarget.rpassword.value) {
+      setErrorMsg(`The passwords don't match`)
+      return
+    }
+
+    try {
+        Router.push('/login')
+    } catch (error) {
+      console.error('An unexpected error happened occurred:', error)
+      setErrorMsg(error.message)
+    }
+  }
+
+  return (
+    <Layout>
+      <div className="login">
+        <Form isLogin={false} errorMessage={errorMsg} onSubmit={handleSubmit} />
+      </div>
+      <style jsx>{`
+        .login {
+          max-width: 21rem;
+          margin: 0 auto;
+          padding: 1rem;
+          border: 1px solid #ccc;
+          border-radius: 4px;
+        }
+      `}</style>
+    </Layout>
+  )
+}
+
+export default Signup

15eefca800dbf274e13bc37186d7b507cf09252e
technoAri <iamari.cse@gmail.com>
2021-11-26 01:44:52 +0530

    code refactors

--- src/helperComponents/Chat/ChatHeader.jsx
+++ src/helperComponents/Chat/ChatHeader.jsx
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import PropTypes from "prop-types";
 import styles from "./ChatMain.module.scss";
+import { useUser } from "../../../lib/hooks";
 
 export default function LeftDrawer() {
+    const user = useUser();
     return (
         <div className={styles.chatheader}>
-            <div>Welcome Ayush</div>
+            <div>Welcome {user.username}</div>
             
             <a href="/api/logout" className={styles.logoutText}>Logout</a>
         </div>

6b0ce4790cd2adaf56adf8d38b88c6b0e5c1135c
Ayush Saurav <ayush.saurva@statrys.com>
2021-12-03 01:03:44 +0530

    API fixes

--- pages/api/profile.js
+++ pages/api/profile.js
@@ -4,7 +4,6 @@ export default async function handler(req, res) {
     if (req.method === 'GET') {
         try {
             const result = await getUserProfile(req.query.query);
-            //const result = await getTotalChat('pesto');
             res.status(200).send({ result });
         } catch (error) {
             console.error(error)
